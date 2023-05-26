0e2e8f6dfdff81fd9c2d5d3dcfe395b385c34bc6
Andrii Sereda <a.a.sereda@ukr.net>
2023-05-26 13:39:48 +0300

    add registranson and verification form with css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { ThemeProvider } from '@emotion/react';
-import { Circles } from 'react-loader-spinner';
 
 import AppBar from './appbar/appbar';
 import { PublicRoute } from './PublicRoute';
@@ -14,13 +13,10 @@ import Verifycation from './verifycation/verification';
 
 import { refreshUser } from 'redux/authOperations';
 import { theme } from 'theme';
-import { getIsLoading, getIsRefreshing } from 'redux/authSelector';
 import SharedLayout from './Shared/shared';
 
 export const App = () => {
   const dispatch = useDispatch();
-  const isLoading = useSelector(getIsLoading);
-  const isRefreshing = useSelector(getIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser());
@@ -29,60 +25,43 @@ export const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-        {/* {isLoading || isRefreshing ? (
-          <>
-            <Circles
-              height="80"
-              width="80"
-              color="#EBD8FF"
-              ariaLabel="circles-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-            />
-            <div>{isLoading}Loading...</div>
-          </>
-        ) : ( */}
-        <>
-          <AppBar />
-          <Routes>
+        <AppBar />
+        <Routes>
+          <Route
+            path="/register"
+            element={<PublicRoute redirectTo="/" component={<Register />} />}
+          />
+          <Route
+            path="/verifycation"
+            element={
+              <PublicRoute redirectTo="/" component={<Verifycation />} />
+            }
+          />
+          <Route
+            path="/login"
+            element={<PublicRoute redirectTo="/" component={<Login />} />}
+          />
+          <Route path="/" element={<SharedLayout />}>
             <Route
-              path="/register"
-              element={<PublicRoute redirectTo="/" component={<Register />} />}
-            />
-            <Route
-              path="/verifycation"
+              path="/cycles"
               element={
-                <PublicRoute redirectTo="/" component={<Verifycation />} />
+                <PrivatRoute redirectTo="/login" component={<Cycles />} />
               }
-            />
+            ></Route>
             <Route
-              path="/login"
-              element={<PublicRoute redirectTo="/" component={<Login />} />}
-            />
-            <Route path="/" element={<SharedLayout />}>
-              <Route
-                path="/cycles"
-                element={
-                  <PrivatRoute redirectTo="/login" component={<Cycles />} />
-                }
-              ></Route>
-              <Route
-                path="/"
-                element={
-                  <PrivatRoute redirectTo="/login" component={<Cycles />} />
-                }
-              ></Route>
-              <Route
-                path="*"
-                element={
-                  <PrivatRoute redirectTo="/login" component={<Cycles />} />
-                }
-              ></Route>
-            </Route>
-          </Routes>
-        </>
-        {/* )} */}
+              path="/"
+              element={
+                <PrivatRoute redirectTo="/login" component={<Cycles />} />
+              }
+            ></Route>
+            <Route
+              path="*"
+              element={
+                <PrivatRoute redirectTo="/login" component={<Cycles />} />
+              }
+            ></Route>
+          </Route>
+        </Routes>
       </ThemeProvider>
     </>
   );
