d577c85b2d0730721dd9616b272157199cf9b63c
Andrii Sereda <a.a.sereda@ukr.net>
2023-06-02 17:00:09 +0300

    add style to genSettingForm

--- src/components/settings/genSetting/genSettingForm.js
+++ src/components/settings/genSetting/genSettingForm.js
@@ -1,6 +1,17 @@
 import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 import shortid from 'shortid';
+import {
+  DatePickerContainer,
+  GeneratorSettingButton,
+  GeneratorSettingFormDataForm,
+  GeneratorSettingFormDataInput,
+  GeneratorSettingFormDataLabel,
+  GeneratorSettingFormDataSpan,
+} from './genSettingFormStyled';
 
 const GeneratorSettingsForm = () => {
   const [brand, setBrand] = useState('');
@@ -18,7 +29,7 @@ const GeneratorSettingsForm = () => {
   const firstChangeOilReglamentId = shortid();
   const nextChangeOilReglamentId = shortid();
   const electricalPowerId = shortid();
-  const dataFirstStartId = shortid();
+  // const dataFirstStartId = shortid();
   const workingFirsStartId = shortid();
   const oilVolumeId = shortid();
   const fuelVolumeId = shortid();
@@ -35,8 +46,8 @@ const GeneratorSettingsForm = () => {
         return setNextChangeOilReglament(value);
       case 'electricalPower':
         return setElectricalPower(value);
-      case 'dataFirstStart':
-        return setDataFirstStart(value);
+      // case 'dataFirstStart':
+      // return setDataFirstStart(value);
       case 'workingFirsStart':
         return setWorkingFirsStart(value);
       case 'oilVolume':
@@ -51,35 +62,39 @@ const GeneratorSettingsForm = () => {
   return (
     <>
       <p>Generator settings form</p>
-      <form style={{ display: 'flex', flexDirection: 'column' }}>
-        <label htmlFor={brandId}>
-          <input
-            placeholder={brand ? brand : 'input brand name generator'}
+      <GeneratorSettingFormDataForm
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <GeneratorSettingFormDataLabel htmlFor={brandId}>
+          <GeneratorSettingFormDataInput
+            placeholder={brand ? brand : 'example: Dnipro-M'}
             type="brand"
             name="brand"
             value={brand}
             onChange={handleChange}
             id={brandId}
           />
-          Brand name generator
-        </label>
-        <label htmlFor={typeId}>
-          <input
-            placeholder={type ? type : 'input generator model'}
+          <GeneratorSettingFormDataSpan>
+            Brand name generator
+          </GeneratorSettingFormDataSpan>
+        </GeneratorSettingFormDataLabel>
+        <GeneratorSettingFormDataLabel htmlFor={typeId}>
+          <GeneratorSettingFormDataInput
+            placeholder={type ? type : 'example: GX-50E'}
             type="type"
             name="type"
             value={type}
             onChange={handleChange}
             id={typeId}
           />
-          Generator model
-        </label>
-        <label htmlFor={firstChangeOilReglamentId}>
-          <input
+          <GeneratorSettingFormDataSpan>
+            Generator model
+          </GeneratorSettingFormDataSpan>
+        </GeneratorSettingFormDataLabel>
+        <GeneratorSettingFormDataLabel htmlFor={firstChangeOilReglamentId}>
+          <GeneratorSettingFormDataInput
             placeholder={
-              firstChangeOilReglament
-                ? firstChangeOilReglament
-                : 'input first change of time oil reglament'
+              firstChangeOilReglament ? firstChangeOilReglament : 'example: 20'
             }
             type="firstOilChange"
             name="firstOilChange"
@@ -87,14 +102,14 @@ const GeneratorSettingsForm = () => {
             onChange={handleChange}
             id={firstChangeOilReglamentId}
           />
-          First change of time oil reglament
-        </label>
-        <label htmlFor={nextChangeOilReglamentId}>
-          <input
+          <GeneratorSettingFormDataSpan>
+            First change of time oil reglament, hours
+          </GeneratorSettingFormDataSpan>
+        </GeneratorSettingFormDataLabel>
+        <GeneratorSettingFormDataLabel htmlFor={nextChangeOilReglamentId}>
+          <GeneratorSettingFormDataInput
             placeholder={
-              nextChangeOilReglament
-                ? nextChangeOilReglament
-                : 'input next change of time oil reglament'
+              nextChangeOilReglament ? nextChangeOilReglament : 'example: 50'
             }
             type="firstOilChange"
             name="firstOilChange"
@@ -102,95 +117,89 @@ const GeneratorSettingsForm = () => {
             onChange={handleChange}
             id={nextChangeOilReglamentId}
           />
-          Next change of time oil reglament
-        </label>
-        <label htmlFor={electricalPowerId}>
-          <input
-            placeholder={
-              electricalPower
-                ? electricalPower
-                : 'input electrical power generator'
-            }
+          <GeneratorSettingFormDataSpan>
+            Next change of time oil reglament, hours
+          </GeneratorSettingFormDataSpan>
+        </GeneratorSettingFormDataLabel>
+        <GeneratorSettingFormDataLabel htmlFor={electricalPowerId}>
+          <GeneratorSettingFormDataInput
+            placeholder={electricalPower ? electricalPower : 'example: 5.5'}
             type="electricalPower"
             name="electricalPower"
             value={electricalPower}
             onChange={handleChange}
             id={electricalPowerId}
           />
-          Electrical power generator
-        </label>
-        <label htmlFor={dataFirstStartId}>
-          <input
+          <GeneratorSettingFormDataSpan>
+            Electrical power generator, kW
+          </GeneratorSettingFormDataSpan>
+        </GeneratorSettingFormDataLabel>
+        {/* <GeneratorSettingFormDataLabel htmlFor={dataFirstStartId}> */}
+        {/* <GeneratorSettingFormDataInput
             placeholder={
-              dataFirstStart
-                ? dataFirstStart
-                : 'input date of first start generator'
+              dataFirstStart ? dataFirstStart : 'example: 05.04.2023'
             }
             type="dataFirstStart"
             name="dataFirstStart"
             value={dataFirstStart}
             onChange={handleChange}
             id={dataFirstStartId}
+          /> */}
+        <DatePickerContainer>
+          <DatePicker
+            selected={dataFirstStart}
+            onChange={date => setDataFirstStart(date)}
+            style={{ ...GeneratorSettingFormDataInput }}
           />
-          Date of first start generator
-        </label>
-        <label htmlFor={workingFirsStartId}>
-          <input
-            placeholder={
-              workingFirsStart
-                ? workingFirsStart
-                : 'input time before first start generator'
-            }
+          <GeneratorSettingFormDataSpan>
+            Date of first start generator
+          </GeneratorSettingFormDataSpan>
+        </DatePickerContainer>
+
+        {/* </GeneratorSettingFormDataLabel> */}
+        <GeneratorSettingFormDataLabel htmlFor={workingFirsStartId}>
+          <GeneratorSettingFormDataInput
+            placeholder={workingFirsStart ? workingFirsStart : 'example: 10'}
             type="workingFirsStart"
             name="workingFirsStart"
             value={workingFirsStart}
             onChange={handleChange}
             id={workingFirsStartId}
           />
-          Time before first start generator
-        </label>
-        <label htmlFor={oilVolumeId}>
-          <input
-            placeholder={
-              oilVolume ? oilVolume : 'input volume of oil generator'
-            }
+          <GeneratorSettingFormDataSpan>
+            Time before first start generator, hours
+          </GeneratorSettingFormDataSpan>
+        </GeneratorSettingFormDataLabel>
+        <GeneratorSettingFormDataLabel htmlFor={oilVolumeId}>
+          <GeneratorSettingFormDataInput
+            placeholder={oilVolume ? oilVolume : 'example: 1.1'}
             type="oilVolume"
             name="oilVolume"
             value={oilVolume}
             onChange={handleChange}
             id={oilVolumeId}
           />
-          Volume of oil generator
-        </label>
-        <label htmlFor={fuelVolumeId}>
-          <input
-            placeholder={
-              fuelVolume ? fuelVolume : 'input volume of fuel tank generator'
-            }
+          <GeneratorSettingFormDataSpan>
+            Volume oil of generator, litre
+          </GeneratorSettingFormDataSpan>
+        </GeneratorSettingFormDataLabel>
+        <GeneratorSettingFormDataLabel htmlFor={fuelVolumeId}>
+          <GeneratorSettingFormDataInput
+            placeholder={fuelVolume ? fuelVolume : 'example: 25'}
             type="fuelVolume"
             name="fuelVolume"
             value={fuelVolume}
             onChange={handleChange}
             id={fuelVolumeId}
           />
-          Volume of fuel tank generator
-        </label>
-      </form>
+          <GeneratorSettingFormDataSpan>
+            Volume of fuel tank generator, litre
+          </GeneratorSettingFormDataSpan>
+        </GeneratorSettingFormDataLabel>
+        <GeneratorSettingButton type="submit">Submit</GeneratorSettingButton>
+      </GeneratorSettingFormDataForm>
     </>
   );
 };
 
 export default GeneratorSettingsForm;
-
-//   const [generatorSetting, setGeneratorSetting] = useState({
-//     brand: null,
-//     type: null,
-//     firstChangeOilReglament: null,
-//     nextChangeOilReglament: null,
-//     electricalPower: null,
-//     dataFirstStart: null,
-//     workingFirsStart: null,
-//     oilVolume: null,
-//     fuelVolume: null,
-//     _id: null,
-//   });
---
+++ src/components/settings/genSetting/genSettingFormStyled.js
@@ -0,0 +1,140 @@
+import styled from '@emotion/styled';
+
+export const GeneratorSettingFormDataForm = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: ${p => p.theme.space[0]};
+  margin-right: ${p => p.theme.space[0]};
+  margin-bottom: ${p => p.theme.space[0]};
+  margin-left: ${p => p.theme.space[0]};
+
+  padding-top: ${p => p.theme.space[0]};
+  padding-right: ${p => p.theme.space[0]};
+  padding-left: ${p => p.theme.space[7]};
+  padding-bottom: ${p => p.theme.space[0]};
+`;
+
+export const GeneratorSettingFormDataLabel = styled.label`
+  height: ${p => p.theme.space[5]};
+  width: ${p => p.theme.space[18]};
+
+  padding: ${p => p.theme.space[0]};
+  margin-top: ${p => p.theme.space[0]};
+  margin-bottom: ${p => p.theme.space[4]};
+`;
+
+export const GeneratorSettingFormDataInput = styled.input`
+  height: ${p => p.theme.space[5]};
+  width: ${p => p.theme.space[14]};
+
+  padding: ${p => p.theme.space[0]};
+  margin: ${p => p.theme.space[0]};
+
+  border: ${p => p.theme.borders.none};
+  border-radius: ${p => p.theme.radii.normal};
+
+  text-align: center;
+
+  color: ${p => p.theme.colors.colorTextDark};
+  background-color: ${p => p.theme.colors.bgColorForm};
+
+  font-family: 'Jura';
+  font-style: normal;
+  font-weight: ${p => p.theme.fontWeights.medium};
+  font-size: ${p => p.theme.fontSizes.l};
+  line-height: ${p => p.theme.fontSizes.xl};
+`;
+export const GeneratorSettingFormDataSpan = styled.span`
+  margin-left: ${p => p.theme.space[4]};
+
+  font-family: 'Jura';
+  font-style: normal;
+  font-weight: ${p => p.theme.fontWeights.medium};
+  font-size: ${p => p.theme.fontSizes.m};
+  line-height: ${p => p.theme.fontSizes.m};
+`;
+export const GeneratorSettingButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: ${p => p.theme.space[12]};
+  height: ${p => p.theme.space[6]};
+
+  padding: ${p => p.theme.space[0]};
+
+  margin-top: ${p => p.theme.space[4]};
+  margin-right: ${p => p.theme.space[0]};
+  margin-bottom: ${p => p.theme.space[0]};
+  margin-left: ${p => p.theme.space[11]};
+
+  border-radius: ${p => p.theme.radii.normal};
+  border: none;
+
+  background-color: ${p =>
+    p.disabled
+      ? p.theme.colors.colorButtonIsDisabled
+      : p.theme.colors.colorButtonNoActive};
+  color: ${p =>
+    p.disabled
+      ? p.theme.colors.colorTextDisabled
+      : p.theme.colors.colorTextDark};
+
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+
+  font-family: 'Jura';
+  font-style: normal;
+  font-weight: ${p => p.theme.fontWeights.semiBold};
+  font-size: ${p => p.theme.fontSizes.l};
+  line-height: ${p => p.theme.fontSizes.xl};
+  text-transform: uppercase;
+
+  transition: 250ms cubic-bezier(0.33, 1, 0.68, 1);
+  transition-property: background-color;
+
+  &:hover,
+  &:focus {
+    background-color: ${p =>
+      p.disabled
+        ? p.theme.colors.colorButtonIsDisabled
+        : p.theme.colors.colorButtonInHover};
+  }
+  cursor: pointer;
+`;
+
+export const DatePickerContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  height: ${p => p.theme.space[5]};
+
+  margin-bottom: ${p => p.theme.space[4]};
+
+  //   > div {
+  //   width: ${p => p.theme.space[14]};
+  //   height: ${p => p.theme.space[5]};
+
+  // > input {
+  //   height: ${p => p.theme.space[5]};
+  //   width: ${p => p.theme.space[14]};
+
+  //   padding: ${p => p.theme.space[0]};
+  //   margin: ${p => p.theme.space[0]};
+
+  //   border: ${p => p.theme.borders.none};
+  //   border-radius: ${p => p.theme.radii.normal};
+
+  //   text-align: center;
+
+  //   color: ${p => p.theme.colors.colorTextDark};
+  //   background-color: ${p => p.theme.colors.bgColorForm};
+
+  //   font-family: 'Jura';
+  //   font-style: normal;
+  //   font-weight: ${p => p.theme.fontWeights.medium};
+  //   font-size: ${p => p.theme.fontSizes.l};
+  //   line-height: ${p => p.theme.fontSizes.xl};
+  // }
+  //   }
+`;
